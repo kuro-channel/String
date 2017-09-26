b153f4578a645b023d2badaa09fa5e2cfa9fc19c
kuro-channel <takumikuroiwa@gmail.com>
2017-09-26 23:04:49 +0900

    9/26 作業分

--- src/String1.java
+++ src/String1.java
@@ -144,4 +144,34 @@ public class String1 {
 	public String nonStart(String a, String b) {
 		return a.substring(1) + b.substring(1);
 	}
+
+	//Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end.
+	// The string length will be at least 2.
+	//
+	//
+	//left2("Hello") → "lloHe"
+	//left2("java") → "vaja"
+	//left2("Hi") → "Hi"
+	public String left2(String str) {
+		if(str.length() <= 2){return str;};
+		// 左から最初の2文字をそれ以降の文字と入れ替える
+		String left2 = str.substring(0, 2);
+		String right = str.substring(2);
+		return right + left2;
+	}
+
+	//Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start.
+	//		The string length will be at least 2.
+	//
+	//
+	//right2("Hello") → "loHel"
+	//right2("java") → "vaja"
+	//right2("Hi") → "Hi"
+	public String right2(String str) {
+		if(str.length() <= 2){return str;};
+		// 右から最初の2文字をそれ以降の文字と入れ替える
+		String right2 = str.substring(str.length()-2);
+		String left = str.substring(0,str.length()-2);
+		return right2 + left;
+	}
 }
