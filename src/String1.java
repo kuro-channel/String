0852154b33a666f40ebf6803b8416e69dd93ecea
kuro-channel <takumikuroiwa@gmail.com>
2017-10-07 23:33:08 +0900

    10/7 作業分

--- src/String1.java
+++ src/String1.java
@@ -238,7 +238,222 @@ public class String1 {
 	// nTwice("Chocolate", 3) → "Choate"
 	// nTwice("Chocolate", 1) → "Ce"
 	public String nTwice(String str, int n) {
-
+		String front = str.substring(0,n);
+		String end = str.substring(str.length()-n);
+		return front + end;
 	}
+
+	//	Given a string and an index, return a string length 2 starting at the given index.
+	//	If the index is too big or too small to define a string length 2,
+	//	use the first 2 chars. The string length will be at least 2.
+	//
+	//	twoChar("java", 0) → "ja"
+	//	twoChar("java", 2) → "va"
+	//	twoChar("java", 3) → "ja"
+	public String twoChar(String str, int index) {
+		// 2文字よりも少ない場合はそのまま返す
+		if(str.length()<2){return str;};
+		String twoChar = "";
+		// index+1がstrの長さ以上の場合、もしくは小さい場合は先頭から2文字を抽出
+		// それ以外の場合は、indexを起点に2文字を抽出
+		if(str.length() <= index +1 || index <0){
+			return str.substring(0,2);
+		}
+		return str.substring(index,index +2);
+	}
+
+	//	Given a string of odd length, return the string length 3 from its middle,
+	//	so "Candy" yields "and". The string length will be at least 3.
+	//
+	//	middleThree("Candy") → "and"
+	//	middleThree("and") → "and"
+	//	middleThree("solving") → "lvi"
+	public String middleThree(String str) {
+		// strが3文字より小さい場合はそのまま返す
+		if(str.length() < 3){return str;}
+		// 奇数の真ん中3文字を抽出する
+		return str.substring((str.length()/2)-1, (str.length()/2)+2);
+	}
+
+	//	Given a string, return true if "bad" appears starting at index 0 or 1 in the string,
+	//	such as with "badxxx" or "xbadxx" but not "xxbadxx".
+	//	The string may be any length, including 0. Note: use .equals() to compare 2 strings.
+	//
+	//	hasBad("badxx") → true
+	//	hasBad("xbadxx") → true
+	//	hasBad("xxbadxx") → false
+	public boolean hasBad(String str) {
+		// strが3文字より小さい場合はそのまま返す
+		if(str.length() < 3){return false;}
+		// 0文字目、または1文字目が"bad"で始まる場合、trueを返す
+		if(str.substring(0,3).equals("bad")){
+			return true;
+		}else if(str.length() >= 4 && str.substring(1,4).equals("bad")){
+			return true;
+		}
+		return false;
+	}
+
+
+	//Given a string, return a string length 2 made of its first 2 chars.
+	//	If the string length is less than 2, use '@' for the missing chars.
+	//
+	//atFirst("hello") → "he"
+	//atFirst("hi") → "hi"
+	//atFirst("h") → "h@"
+	public String atFirst(String str) {
+		// 2文字よりも少ない場合、"@"をつける
+		switch (str.length()) {
+		case 0:
+			return "@@";
+		case 1:
+			return str +"@";
+		default:
+			break;
+		}
+		// 先頭2文字を抽出して返す
+		return str.substring(0, 2);
+	}
+
+	//	Given 2 strings, a and b, return a new string made of the first char of a and the last char of b,
+	//	so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+	//
+	//	lastChars("last", "chars") → "ls"
+	//	lastChars("yo", "java") → "ya"
+	//	lastChars("hi", "") → "h@"
+	public String lastChars(String a, String b) {
+		// 共に空文字だった場合は"@@"を返す
+		if(a.length() == 0 && b.length() == 0){
+			return "@@";
+		}
+		// aかb、いずれかの長さが0の場合は"@"をつける
+		if(a.length() == 0){
+			a = "@";
+		}else if(b.length() == 0){
+			b = "@";
+		}
+		return a.substring(0,1) + b.substring(b.length()-1, b.length());
+	}
+
+	// Given two strings, append them together (known as "concatenation") and return the result.
+	// However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+	//
+	// conCat("abc", "cat") → "abcat"
+	// conCat("dog", "cat") → "dogcat"
+	// conCat("abc", "") → "abc"
+	public String conCat(String a, String b) {
+		// aとbいずれかが空文字の場合
+		if(a.length() == 0 || b.length() == 0){
+			return a + b;
+		}
+		// aの接尾語とbの接頭語が一致していた場合、文字を重ねて結合する
+		if(a.substring(a.length()-1).equals(b.substring(0,1))){
+			return a + b.substring(1);
+		}
+		return a + b;
+	}
+
+	//	Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign".
+	//
+	//	lastTwo("coding") → "codign"
+	//	lastTwo("cat") → "cta"
+	//	lastTwo("ab") → "ba"
+	public String lastTwo(String str) {
+		// 2文字よりも短い場合、strをそのまま返る
+		if(str.length() < 2){return str;};
+		// 接頭語2文字を入れ替える
+		String lastTwo = str.substring(str.length()-1) +
+				str.substring(str.length()-2, str.length()-1);
+		return str.substring(0,str.length()-2) + lastTwo;
+	}
+	//	Given a string, if the string begins with "red" or "blue" return that color string,
+	//			otherwise return the empty string.
+
+	//	seeColor("redxx") → "red"
+	//	seeColor("xxred") → ""
+	//	seeColor("blueTimes") → "blue"
+	public String seeColor(String str) {
+		// red/blueで始まる場合、それぞれred/blueの文字列を返す
+		if(str.startsWith("red")){
+			return "red";
+		}else if(str.startsWith("blue")){
+			return "blue";
+		}
+		return "";
+	}
+	//	Given a string, return true if the first 2 chars in the string also appear
+	//	at the end of the string, such as with "edited".
+	//
+	//	frontAgain("edited") → true
+	//	frontAgain("edit") → false
+	//	frontAgain("ed") → true
+	public boolean frontAgain(String str) {
+		// 2文字より短い場合はstrを返す
+		if(str.length() < 2){return false;};
+		// 先頭2文字を抽出し、抽出した先頭2文字と同じ文字列があった場合、trueを返す
+		if(str.substring(0, 2).equals(str.substring(str.length()-2))){
+			return true;
+		}
+		return false;
+	}
+
+	// Given two strings, append them together (known as "concatenation") and return the result.
+	// However, if the strings are different lengths, omit chars from the longer string
+	// so it is the same length as the shorter string. So "Hello" and "Hi" yield "loHi". The strings may be any length.
+	//
+	// minCat("Hello", "Hi") → "loHi"
+	// minCat("Hello", "java") → "ellojava"
+	// minCat("java", "Hello") → "javaello"
+	public String minCat(String a, String b) {
+		String result = "";
+		int dif = 0;
+		// aとbの文字数を比較し、少ない方を基準とする
+		if(a.length() > b.length()){
+			dif = a.length() - b.length();
+			result = a.substring(dif) + b;
+		}else if(a.length() < b.length()){
+			dif = b.length() - a.length();
+			result = a + b.substring(dif);
+		}
+		return result;
+	}
+
+	//	Given a string, return a new string made of 3 copies of the first 2 chars of the original string.
+	//	The string may be any length. If there are fewer than 2 chars, use whatever is there.
+	//
+	//	extraFront("Hello") → "HeHeHe"
+	//	extraFront("ab") → "ababab"
+	//	extraFront("H") → "HHH"
+	public String extraFront(String str) {
+		// strが2文字以下の場合、strを3回結合する
+		String result = "";
+		int count = 0;
+		if(str.length()<=2){
+			while(count <3){
+				result = result + str;
+				count++;
+			}
+		}
+		// strが3文字以上の場合、先頭2文字を抽出して3回結合する
+		if(str.length() >=3){
+			while(count <3){
+				result = result + str.substring(0, 2);
+				count++;
+			}
+		}
+		return result;
+	}
+
+	//Given a string, if a length 2 substring appears at both its beginning and end,
+	//return a string without the substring at the beginning, so "HelloHe" yields "lloHe".
+	//		The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
+	//
+	//
+	//without2("HelloHe") → "lloHe"
+	//without2("HelloHi") → "HelloHi"
+	//without2("Hi") → ""
+//	public String without2(String str) {
+//
+//	}
 
 }
